5f36332dc54d2835ae513bf1dc749da2e6836e54
LeShoyaStarkey <olstarkey@gmail.com>
2016-02-16 14:30:10 -0800

    took out pycache files and added javascript files to assets

--- city_scape_project/urls.py
+++ city_scape_project/urls.py
@@ -15,8 +15,11 @@
 # """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^cityscape/', include('cityscape.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+    
+urlpatterns += staticfiles_urlpatterns()
