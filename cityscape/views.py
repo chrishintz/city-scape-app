eb737cf3c2f54efd786a1acc9a3a169f930b317d
Chris Hintz <chrishintz42@gmail.com>
2016-02-16 16:24:46 -0800

    current page not found error

--- cityscape/views.py
+++ cityscape/views.py
@@ -5,5 +5,5 @@ from jinja2 import Environment, PackageLoader
 def index(request):
     env = Environment(loader=PackageLoader('cityscape', 'templates'))
     template = env.get_template('jinja2/index.html')
-    print template.render('jinja2/index.html')
+    print(template.render('jinja2/index.html'))
     # HttpResponse("Hello, World")
