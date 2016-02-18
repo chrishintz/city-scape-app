9d122fb8773880ad3cfb87b51d95ae47366134d5
smurfik <smurfik@mlpinit.com>
2016-02-18 15:25:14 -0800

    add algorithmia class with search method

---
+++ cityscape/algorithm.py
@@ -0,0 +1,9 @@
+import Algorithmia
+from django.conf import settings
+
+class Algorithm:
+    @classmethod
+    def search(self, tweet_content, algo_path):
+        client = Algorithmia.client(settings.ALGORITHMIA_API_KEY)
+        algo = client.algo(algo_path)
+        return (algo.pipe(tweet_content))
