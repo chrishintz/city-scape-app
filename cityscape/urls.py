95d07340eb5393d7ae7c7b327d33ba34ebb2413f
LeShoyaStarkey <olstarkey@gmail.com>
2016-02-24 14:08:23 -0800

    commit for merge

--- cityscape/urls.py
+++ cityscape/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^pets$', views.pet_score),
     url(r'^influx', views.influx, name= 'influx'),
     url(r'^traffic', views.traffic, name='traffic'),
-    url(r'^chart$', views.chart, name='chart')
+ +    url(r'^chart$', views.chart, name='chart')
+    url(r'^traffic', views.traffic, name='traffic')
+
 
 ]
