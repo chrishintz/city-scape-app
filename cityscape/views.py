8951edd023177e95674b43e7de0b74aaa0b05908
Chris Hintz <chrishintz42@gmail.com>
2016-02-24 11:14:12 -0800

    stashing changes

--- cityscape/views.py
+++ cityscape/views.py
@@ -9,11 +9,12 @@ from cityscape.weather import Weather
 def index(request):
     env = Environment(loader=PackageLoader('cityscape', 'templates'))
     template = env.get_template('jinja2/index.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(weather = ''.join((Weather.current_weather(), ".png"))))
+
+    # return HttpResponse(template.render())
 
 def pet_score(request):
     return JsonResponse(Pets.recent_average(), safe=False)
 
 # def weather(request):
 #     return HttpResponse(Weather.recent_tweets().to_s())  ##doesn't work, needs to be a string
-    return HttpResponse(template.render(weather = ''.join((Weather.current_weather(), ".png"))))
