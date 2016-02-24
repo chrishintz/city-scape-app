e1f9f01cbf43e39321a763f20aee382e87bb4abc
Evelyn <evieb16@gmail.com>
2016-02-23 16:19:16 -0800

    Adds weather images to images file, renders current weather image in view

--- cityscape/weather.py
+++ cityscape/weather.py
@@ -1,39 +1,20 @@
-# Create a class
-# Search twitter for tweets related to Weather
-# Save each relevant tweet in MongoDB with my score
-# Allow for researching twitter using the most recent tweet id
-# Aggregate data into a dingle score for a time period
-
 from cityscape.tweet import Tweet
 from cityscape.mongo import Mongo
 import pymongo
 from datetime import datetime, timedelta
+import re
 
 class Weather:
 
     dictionary = {
         "rainy": ["rainy", "raining", "rain", "showers", "drizzle", "drizzling", "sprinkling"],
-        "windy": ["windy", "breezy", "wind"],
-        "sunny": ["sunny", "sun"],
+        "windy": ["windy", "breezy", "wind", "winds"],
+        "sunny": ["sunny", "sun", "partly cloudy"],
         "snowy": ["snowy", "snow"],
         "cloudy": ["cloudy", "clouds", "overcast"],
         "thunder": ["thunder"],
         "lightening": ["lightening"]
     }
-
-    @classmethod
-    def average(self):
-        pipe = [{"$match": {"module": "Weather"}}, {'$group': {'_id': None, 'total': {'$avg': '$score'}}}]
-        agg = Mongo.collection.aggregate(pipeline=pipe)
-        return list(agg)
-
-    @classmethod
-    def recent_average(self):
-        start = datetime.today() - timedelta(hours = 24)
-        end = datetime.today()
-        pipe = [{"$match": {"module": "Weather", "published_at": {"$gte": start, "$lte": end}}}, {'$group': {'_id': None, 'total': {'$avg': '$score'}}}]
-        agg = Mongo.collection.aggregate(pipeline=pipe)
-        return list(agg)
 
     @classmethod
     def update_data(self):
@@ -44,7 +25,7 @@ class Weather:
         else:
             since_id = None
 
-        tweets = Tweet.search("-filter:links -filter:retweets geocode:47.609403608607785,-122.35061645507812,6mi", count=100, since_id=since_id)
+        tweets = Tweet.search("-filter:links -filter:retweets geocode:47.609403608607785,-122.35061645507812,6mi", count=20000, since_id=since_id)
 
         for tweet in tweets:
             count_dictionary = {}
@@ -52,14 +33,25 @@ class Weather:
             for key in Weather.dictionary:
                 count_dictionary[key] = 0
 
-            for word in tweet.text.split():
-                for key, value in Weather.dictionary.items():
-                    if word in value:
-                        count_dictionary[key] += 1
+            # without regex
+            # for word in tweet.text.split():
+            #     for key, value in Weather.dictionary.items():
+            #         if word in value:
+            #             count_dictionary[key] += 1
 
-            # percentage = (float(upper_count) / float(len(tweet.text))) * 100
-            # if percentage < 20: percentage = 0
-            # score = figure out the score for the current weather
+            # with regex
+            for key, value_list in Weather.dictionary.items():
+                for value in value_list:
+                    if value == "wind":
+                        wind_mph = Weather.extract_wind_in_mph(tweet.text.lower())
+
+                        # increment count only if wind is above threshhold
+                        if (25 <= wind_mph):
+                            count_dictionary[key] += 1
+                    else:
+                        if re.search(value, tweet.text.lower()):
+                            count_dictionary[key] += 1
+
             Mongo.collection.insert_one({
                 "module": "Weather",
                 "published_at": tweet.created_at,
@@ -76,9 +68,18 @@ class Weather:
         for key in Weather.dictionary:
             total_count_dictionary[key] = 0
 
-        for item in Mongo.collection.find():
+        for item in Mongo.collection.find({"published_at": {"$gte": datetime.today() - timedelta(hours = 3)}}):
             for key, value in item["score"].items():
                 total_count_dictionary[key] += value
 
-        print(max(total_count_dictionary, key=total_count_dictionary.get))
+        return max(total_count_dictionary, key=total_count_dictionary.get)
         # print(total_count_dictionary)
+
+    @classmethod
+    def extract_wind_in_mph(self, tweet_content):
+        result = re.search("wind (.+?)mph", tweet_content)
+
+        if result:
+            return int(result.group(1))
+        else:
+            return 0
