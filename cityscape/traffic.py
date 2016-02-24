38e27f4cc9ecfdf776f77367efc3f54bb0c18886
Karen Hambro <khambro@gmail.com>
2016-02-24 10:39:33 -0800

    adds traffic comparison

--- cityscape/traffic.py
+++ cityscape/traffic.py
@@ -4,39 +4,57 @@ import pymongo
 from datetime import datetime, timedelta
 
 
+
 class Traffic:
 
     @classmethod
     def average(self):
-        pipe = [{"$match": {"module": "Traffic"}}, {'$group': {'_id': None, 'total': {'$avg': '$score'}}}]
-        agg = Mongo.collection.aggregate(pipeline=pipe)
-        return list(agg)
+        pipe = [{"$match": {"module": "Traffic"}}, {
+        '$group': {
+            '_id': { "month": { "$month": "$published_at" }, "hour": {"$hour": "$published_at"}, "day": {"$dayOfMonth": "$published_at"}, "year": { "$year": "$published_at" }},
+            "total": {'$sum': 1 }
+        }} ]
+        #will need to count by hour
+        offset = 8
+        real_count = []
+        count = Mongo.collection.aggregate(pipeline=pipe)
+        for everything in count:
+            if -2 <= everything["_id"]["hour"]-offset <= 14:
+                real_count.append(everything)
+        sum = 0
+        for hour in real_count:
+            if -2 <= hour["_id"]["hour"]-offset <= 14:
+                sum += hour["total"]
+        return sum/ len(real_count)
+             #how to determine if a number is in a range?? #then sum it if so. How many thing summing so I can divide by that number
 
     @classmethod
-    def recent_average(self, hours=24):
+    def recent_average(self, hours=1):
         start = datetime.today() - timedelta(hours = hours)
         end   = datetime.today()
-        pipe = [{"$match": {"module": "Traffic", "published_at": {"$gte": start, "$lte": end}}}, {'$group': {'_id': None, 'total': {'$avg': '$score'}}}]
-        agg = Mongo.collection.aggregate(pipeline=pipe)
-        return list(agg)
+        count = Mongo.collection.find({"published_at": {"$gte": start, "$lte": end} }).count()
+        return count
+
+    @classmethod
+    def comparison(self):
+        return self.recent_average()/self.average()    
 
 
     @classmethod
     def update_data(self):
         newest_tweet = Mongo.collection.find({"module": "Traffic"}).sort("published_at", pymongo.DESCENDING)
-        if newest_tweet:
+        if newest_tweet.count() > 0:
             since_id = newest_tweet[0]["tweet_id"]
         else:
             since_id = None
         print(since_id)
         tweets = Tweet.search(
-        "seattle traffic -filter:links -filter:retweets geocode:47.609403608607785,-122.35061645507812,25mi",
-        count=1000,
+        "seattle traffic",
+        count=20000,
         since_id=since_id
         )
         # count = len(tweets)
         for tweet in tweets:
-
             Mongo.collection.insert_one({
                 "module": "Traffic",
                 "published_at": tweet.created_at,
@@ -44,5 +62,11 @@ class Traffic:
                 "tweet_id": tweet.id,
                 "score": 1
         })
+
+        @classmethod
+        def comparison(self):
+            return self.recent_average()/self.average()
+
+
 
         return True
