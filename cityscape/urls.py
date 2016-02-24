caf14b3a68f68f9a41db6083e047f095a182760e
smurfik <smurfik@mlpinit.com>
2016-02-24 10:11:55 -0800

    add route for the chart

--- cityscape/urls.py
+++ cityscape/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from . import views
+from . import happy
 
 urlpatterns = [
     # url(r'^weather', views.index, name='weather'),
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^chart$', views.chart, name='chart'),
 ]
