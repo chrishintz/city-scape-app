de261d9281e12bffff5e92121b698745e4d7163e
danieladler <weston.adler@gmail.com>
2016-02-24 11:33:53 -0800

    start implementing time class

--- cityscape/time.py
+++ cityscape/time.py
@@ -7,10 +7,17 @@ from cityscape.tweet import Tweet
 from cityscape.mongo import Mongo
 import pymongo
 from datetime import datetime, timedelta
+import re
 
 class Time:
 
-    # add @classmethods for score/accuracy later
+    dictionary = {
+        "early":     ["early","insomnia"],
+        "morning":   ["morning", "breakfast", "awake"],
+        "afternoon": ["afternoon","lunch"],
+        "evening":   ["night", "dinner", "tired"],
+        "late" :     ["midnight","dark"]
+    }
 
     @classmethod
     def update_data(self):
@@ -28,44 +35,100 @@ class Time:
             since_id=since_id
         )
 
-        # create morning/noon/night categories
+        # GOAL: assign score for likely time to each tweet
         for tweet in tweets:
-            # if tweet text contains references to breakfast or sun --> morning_tweets array
-                # OPTION A:
-                # Mongo.collection.insert_one(
-                    # just add new record for this tweet w/ keys for tweet_id and time_guess
-
-                    # then, after all the if/elses determining time of day are done,
-                    # UPDATE the same tweet (while still in the 'for' loop w/ remaining attributes
-                    # (published_at, content, module) – as these modules apply to all tweets
 
 
-                # OPTION B:
-                    #   for tweet in tweets:
-                    #     time_guess = ""
-                    #     if tweet has "breakfast"
-                    #         time_guess = "breakfast"
-                    #     elif tweet has "lunch"
-                    #         time_guess = "lunch"
+            # PASS IN THESE METHODS – which need to be specified outside of the update_data() method
+            # count_dictionary = Time.assign_scores(tweet.text)
+                # this method needs to return the count_dict containing scores
 
-                    # if tweet text contains references to lunch --> daytime_tweets array
-                    # repeat for evening
-                    # repeat for night
-                    # repeat for late night
+            # time_guess = Time.analyze_text(tweet.count_dictionary)
+                # this method gets the highest score from count_dictionary and assigns a time guess
 
-                    # if it doesn't match any of the time_guess criteria, SKIP saving to MongoDB (explicitly tell Python to do this)
+            # check_acuracy = Time.check_accuracy(tweet.time_guess)
+                # compare time guess to published_at
+
+
+            #each tweet starts with an empty hash of possible time guesses, which we're calling 'count dictionary'
+            count_dictionary = {}
+
+            for key in Time.dictionary:
+                # and then each key is assigned to the count dictionary, with a starting value of 0
+                count_dictionary[key] = 0
+
+                ####### non-regex approach:
+
+            # split the tweet's content into a bunch of words, examine each word
+            for word in tweet.text.split():
+                # iterate through the dictionary keys...
+                for key, value in Time.dictionary.items():
+                    # if the word appears in the value list...
+                    if word in value:
+                        # add 1 to the score for that key in the dictionary! (for THIS TWEET ONLY)
+                        count_dictionary[key] += 1
+
+                            # the above loop will happen for each word in each tweet,
+                            # so tweets may have count_dictionaries with scores of
+                            # multiple 1's, 0's or anything else.
+
+                            # later, make it so that if the entire count_dictionary score is
+                            # 0, the tweet isn't even saved in the dictionary.
+
+                    ####### regex approach:
+                    # iterate through the dictionary keys...
+                    # for key, value_list in Time.dictionary.items():
+                    #     # for each value in each list of values belonging to a key
+                    #     for value in value_list:
+                    #
+                    #         if re.search(value, tweet.text.lower()):
+                    #             count_dictionary[key] += 1
+                    #         # else
+                                # skip and do not save in db
+
+                    # FUTURE REFACTOR:  just assign a time_guess to a tweet, skipping assignment
+                    # of a bunch of empty scores to each tweet.
+                        #   for tweet in tweets:
+                        #     time_guess = ""
+                        #     if tweet has "breakfast"
+                        #         time_guess = "breakfast"
+                        #     elif tweet has "lunch"
+                        #         time_guess = "lunch"
+
+                        # if tweet text contains references to lunch --> daytime_tweets array
+                        # repeat for evening
+                        # repeat for night
+                        # repeat for late night
+
+                        # if it doesn't match any of the time_guess criteria, SKIP saving to MongoDB (explicitly tell Python to do this)
 
             Mongo.collection.insert_one({
                 "module": "Time",
                 "published_at": tweet.created_at,
                 "content": tweet.text,
                 "tweet_id": tweet.id,
-                "time_guess": time_guess,
-                "guess_is_accurate": ""
-                # guess_is_accurate - assign as empty string , method below will update to Boolean
+                "score": count_dictionary
+                # "time_guess": time_guess,
+                # "guess_is_accurate": ""
+                    # guess_is_accurate - assign as empty string , method below will update to Boolean
             })
 
         return True
+
+        ##### instance methods:
+        ######## (don't need to take arguments)
+        # count_dictionary = Time.assign_scores(tweet.text)
+            # this method needs to return the count_dict containing scores
+
+        # time_guess = Time.analyze_text(tweet.count_dictionary)
+            # this method gets the highest score from count_dictionary and assigns a time guess
+
+        # check_acuracy = Time.check_accuracy(tweet.time_guess)
+            # compare time guess to published_at
+
+
+
+    # add @classmethods for score/accuracy later
 
     # @classmethod
     # def guess_what_time(self):
